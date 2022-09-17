821490e04d9a62425cd89c70c25b2b1a3103ac6c
Shayan Goudarzi <95754545+ShGoudarzi@users.noreply.github.com>
2022-09-17 09:38:12 +0430

    Update backup-assistant.sh

--- backup-assistant.sh
+++ backup-assistant.sh
@@ -3,12 +3,11 @@ DATE=$(date +"%Y%m%d")
 NOW=$(date +"%d/%b/%Y:%H:%M:%S %:::z")
 HOSTNAME=$(hostname -s)
 LOG_FILE="/var/log/backup-assistant.log"
+tmp_log="/tmp/ftp-bash-fullbackup.log"
 
 pidfile=/var/run/ba.sh.pid
 config_path="/etc/backup-assistant"
 intervalConfigFiles_path="$config_path/source"
-tmp_path="/tmp/backup-assistant"
-tmp_log="/tmp/ftp-bash-fullbackup.log"
 
 week_day=$(date +"%u") 
 week_backup_day="5" # 5:friday
@@ -16,7 +15,6 @@ week_backup_day="5" # 5:friday
 month_day=$(date +"%d")
 month_backup_day="28"
 
-G_path=""
 G_daily_fileName="backup.daily"
 G_weekly_fileName="backup.weekly"
 G_monthly_fileName="backup.monthly"
@@ -75,19 +73,14 @@ function config_check() {
     then
         mkdir -p $intervalConfigFiles_path > /dev/null 2>&1
         cat <<EOF > $config_path/main.conf
-# 1 is for just save backup file locally
-# 2 is for both save locally and send to ftp server
-# 3 is for just send backup file to ftp server
-### Mode ###
-mode="1"
-
-### Encryption ###
-encryption_enable="no"
-encryption_name="$HOSTNAME"
 
 ### Path ###
 local_save_path="/local-backup/"
 ftp_save_path="/ftp-backup/"
+
+### Encryption ###
+encryption_enable="no"
+encryption_name="$HOSTNAME"
 
 ### FTP ###
 ftp_enable="no"
@@ -160,32 +153,11 @@ done
 
 if [ -z "$i_config_files" ]; 
 then
-    echo -e "$NOW Not found any source config files!"
+    echo -e "Not found any source config files or today is not the backup day!" | log_print
     exit 0
 fi
 
 
-
-case $mode in 
-
-    "1" | "2")
-       G_path=$local_save_path
-       ;;
-
-    "3")
-       G_path=$tmp_path
-       ;; 
-
-    *)
-       echo -e "$NOW unknown"
-       exit 0
-       ;;
-
-esac
-
-yellow;
-echo -e "Mode: $mode" | log_print
-resetcolor;
 
 sleep 1;
 ###########################
@@ -194,13 +166,15 @@ sleep 1;
 
 
 # Create Backup
+yellow;
 echo "Creating Backup archives..." | log_print
+resetcolor;
 
 i_result_files=()
 for source_file in ${i_config_files[@]};
 do
     ext=$(echo $source_file | cut -d "." -f 2)
-    result_pre_path=$G_path/$ext
+    result_pre_path=$local_save_path/$ext
     mkdir -p $result_pre_path > /dev/null 2>&1
 
     source_file_path=$intervalConfigFiles_path/$source_file
@@ -211,13 +185,18 @@ do
     i_result_files+=($result)
       
 done
+green;
 echo -e "Backup archives has have been created successfully" | log_print
+resetcolor;
+
 
 
 # GPG Encryption 
 if [ "$encryption_enable" == "yes" ];
 then
+    yellow;
     echo -e "Encrypting Backup archives..." | log_print
+    resetcolor;
 
     counter=0
     for res in ${i_result_files[@]};
@@ -226,7 +205,9 @@ then
 
         if [ $? -ne 0 ]
         then
+            red;
             echo -e "Encrypting backup archives FAILD" | log_print
+            resetcolor;
             exit 0;      
         fi
 
@@ -236,8 +217,9 @@ then
 
     done
 
+    green;
     echo -e "Encrypting backup archives has have been completed successfully" | log_print
-
+    resetcolor;
 fi
 
 
@@ -248,26 +230,32 @@ sleep 1;
 
 
 ### FTP Upload
-if [ "$ftp_enable" == "yes" ] && [ "$mode" != "1" ];
+if [ "$ftp_enable" == "yes" ];
 then
+    yellow;
     echo -e "Uploading to FTP-server..." | log_print
+    resetcolor;
 
     for res in ${i_result_files[@]};
     do  
-        input=$(echo $G_path | sed 's/\//\\\//g')
+        input=$(echo $local_save_path | sed 's/\//\\\//g')
         ftp_save_path=$(echo $ftp_save_path | sed 's/\//\\\//g')
         path=$(dirname $res | sed -e "s/$input/$ftp_save_path/")
         curl --show-error --connect-timeout 30 --retry 5 --retry-delay 60 --upload-file $res --ftp-create-dirs "ftp://$ftp_server:21/$path/" --user "$ftp_username:$ftp_password" 
        
         if [ $? -ne 0 ]
         then
+            red;
             echo -e "Uploading to FTP-server FAILD" | log_print
+            resetcolor;
             exit 0;      
         fi
 
     done
 
+    green;
     echo -e "Uploading to FTP-server has have been completed successfully." | log_print
+    resetcolor;
 
 fi
 
@@ -279,38 +267,41 @@ sleep 1;
 
 
 ### Local
-if [ $G_path != $tmp_path ];
+yellow;
+echo -e "Cleaning local backups older than $local_save_last_versions last old versions..." | log_print
+resetcolor;
+
+i_local_queue=()
+for source_file in ${i_config_files[@]};
+do
+  source_file=$(echo $source_file | cut -d "." -f 2)
+  local_path_dir=$local_save_path/$source_file
+
+  count=$(( $(ls -ltr $local_path_dir | tail -n +2 | wc -l) - $local_save_last_versions ))
+  files=$(ls -ltr $local_path_dir | tail -n +2 | head -n $count | awk '{print $NF}' | awk -F ' ' -v awklocal_path_dir="$local_path_dir/" '{print awklocal_path_dir $1}' | xargs)
+  i_local_queue+=($files)
+
+done
+
+if [ -z "$i_local_queue" ]; 
 then
-    echo -e "Cleaning local backups older than $local_save_last_versions old versions..." | log_print
-
-    i_local_queue=()
-    for source_file in ${i_config_files[@]};
-    do
-        source_file=$(echo $source_file | cut -d "." -f 2)
-        local_path_dir=$local_save_path/$source_file
-
-        count=$(( $(ls -ltr $local_path_dir | tail -n +2 | wc -l) - $local_save_last_versions ))
-        files=$(ls -ltr $local_path_dir | tail -n +2 | head -n $count | awk '{print $NF}' | awk -F ' ' -v awklocal_path_dir="$local_path_dir/" '{print awklocal_path_dir $1}' | xargs)
-        i_local_queue+=($files)
-
-    done
-
-    if [ -z "$i_local_queue" ]; 
-    then
-        echo -e "ّNot need." | log_print
-    else
-        rm -rf ${i_local_queue[@]}
-        echo -e "ّFiles: $files" | log_print
-    fi
-
-    echo -e "Cleaning Done." | log_print
+    echo -e "ّNot need." | log_print
+else
+    rm -rf ${i_local_queue[@]}
+    echo -e "ّFiles: $files" | log_print
 fi
+
+green;
+echo -e "Cleaning Done." | log_print
+resetcolor;
 
 
 ### FTP
-if [ "$ftp_enable" == "yes" ] && [ "$mode" != "1" ];
+if [ "$ftp_enable" == "yes" ];
 then
-    echo -e "Cleaning remote ftp backups older than $ftp_save_last_versions old versions..." | log_print
+    yellow;
+    echo -e "Cleaning remote ftp backups older than $ftp_save_last_versions last old versions..." | log_print
+    resetcolor;
 
     i_ftp_queue=()
     for source_file in ${i_config_files[@]};
@@ -318,7 +309,7 @@ then
         sleep 1
 
         source_file=$(echo $source_file | cut -d "." -f 2)
-        local_path_dir=$G_path/$source_file
+        local_path_dir=$local_save_path/$source_file
  
         ftp_path_dir=$ftp_save_path/$source_file
 
@@ -348,7 +339,9 @@ EOMYF
 
         if [ $? -ne 0 ]
         then
+            red;
             echo -e "Cleaning FTP-server FAILD" | log_print
+            resetcolor;
             exit 0;      
         fi
 
@@ -358,7 +351,10 @@ EOMYF
         echo -e "ّNot need." | log_print
     fi
 
+    green;
     echo -e "Cleaning FTP-server Done." | log_print
+    resetcolor;
+
 fi
 
 
@@ -367,18 +363,21 @@ fi
 
 ########## Final ###########
 echo -e "" | log_print
+green;
 echo -e "*** Backup finished ***" | log_print
+resetcolor;
 echo -e "Backup files:" | log_print
 
 for res in ${i_result_files[@]};
 do
     res=$(echo $res | sed "s/\/\//\//")
     archive_size=$(ls -lh $res | awk '{print  $5}')
+    yellow;
     echo -e "$res : $archive_size" | log_print
-     
+    resetcolor;
 done
 
-rm -rf $tmp_path $tmp_log
+rm -rf $tmp_log
 exit 0
 
 
